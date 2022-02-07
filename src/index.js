ff2bc408cda811c29c1b259d4869622c68c424c6
OrelAlon <orela231089@gmail.com>
2022-02-07 22:13:18 +0200

    commit

--- src/index.js
+++ src/index.js
@@ -9,3 +9,6 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
+
+// REACT_APP_GITHUB_URL="https://api.github.com"
+// REACT_APP_GITHUB_TOKEN='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'
