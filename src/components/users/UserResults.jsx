279ca65bb54408e3df7686c9583a1c42e247b7b2
OrelAlon <orela231089@gmail.com>
2022-02-07 20:22:03 +0200

    repo

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,20 +1,16 @@
-import { useEffect, useContext } from "react";
+import { useContext } from "react";
 import UserItem from "./UserItem";
 import Spinner from "../Spinner";
-import { GithubContext } from "../../context/GithubContext";
+import GithubContext from "../../context/GithubContext";
 
 function UserResults() {
-  const { users, loading, fetchUsers } = useContext(GithubContext);
-
-  useEffect(() => {
-    fetchUsers();
-  }, []);
+  const { users, loading } = useContext(GithubContext);
 
   if (!loading) {
     return (
       <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
         {users.map((user) => (
-          <UserItem key={user.id} user={user}></UserItem>
+          <UserItem key={user.id} user={user} />
         ))}
       </div>
     );
--- src/context/GithubContext.js
+++ src/context/GithubContext.js
@@ -1,30 +1,23 @@
-import { createContext, useState } from "react";
+import { createContext, useReducer } from "react";
+import githubReducer from "./GithubReducer";
 
 const GithubContext = createContext();
 
-const GITHUB_URL = process.env.REACT_APP_GITHUB_URL;
-const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
-
 export const GithubProvider = ({ children }) => {
-  const [users, setUsers] = useState([]);
-  const [loading, setLoading] = useState(true);
-
-  const fetchUsers = async () => {
-    const response = await fetch(`${GITHUB_URL}/users`, {
-      headers: {
-        Authorization: `token ${GITHUB_TOKEN}`,
-      },
-    });
-    const data = await response.json();
-    setUsers(data);
-    setLoading(false);
+  const initialState = {
+    users: [],
+    user: {},
+    repos: [],
+    loading: false,
   };
+
+  const [state, dispatch] = useReducer(githubReducer, initialState);
+
   return (
     <GithubContext.Provider
       value={{
-        users,
-        loading,
-        fetchUsers,
+        ...state,
+        dispatch,
       }}
     >
       {children}
