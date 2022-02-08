69242eb97e9810382773ad1000063dfadc3badc2
OrelAlon <orela231089@gmail.com>
2022-02-08 09:13:18 +0200

    update

--- src/context/GithubAction.js
+++ src/context/GithubAction.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const GITHUB_URL = process.env.REACT_APP_GITHUB_URL;
-const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
+// const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
 
 const github = axios.create({
   baseURL: GITHUB_URL,
