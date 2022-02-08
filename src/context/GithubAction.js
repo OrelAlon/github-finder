94c3c78caaf275087b6a82bd407751bb81f79df7
OrelAlon <orela231089@gmail.com>
2022-02-08 08:46:49 +0200

    update githube action

--- src/context/GithubAction.js
+++ src/context/GithubAction.js
@@ -5,7 +5,7 @@ const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
 
 const github = axios.create({
   baseURL: GITHUB_URL,
-  headers: { Authorization: `token ${GITHUB_TOKEN}` },
+  // headers: { Authorization: `token ${GITHUB_TOKEN}` },
 });
 
 // Get search results
